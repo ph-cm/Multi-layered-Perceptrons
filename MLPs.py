6ffdde9f505068375d52bd59b6c6888001ec03ad
Pedro Henrique <trabalho.prog.pedro@gmail.com>
2024-11-11 03:34:56 -0300

    Loss Minimization Problem and Network Training

--- MLPs.py
+++ MLPs.py
@@ -128,3 +128,50 @@ z = net.forward(train_x[0:10])
 p = softmax.forward(z)
 loss = cross_ent_loss.forward(p, train_labels[0:10])
 print(loss)
+
+#Loss Minimization Problem nd Network Training
+class Linear:
+    def __init__(self, nin, nout):
+        self.W = np.random.normal(0, 1.0/np.sqrt(nin), (nout, nin))
+        self.b = np.zeros((1,nout))
+        self.dW = np.zeros_like(self.W)
+        self.db = np.zeros_like(self.b)
+    
+    def forward(self, x):
+        self.x = x 
+        return np.dot(x, self.W.T) + self.b
+    
+    def backward(self, dz):
+        dx = np.dot(dz, self.W)
+        dW = np.dot(dz.T, self.x)
+        db = dz.sum(axis = 0)
+        self.dW = dW
+        self.db = db
+        return dx
+
+    def update(self, lr):
+        self.W -= lr*self.dW
+        self.b -= lr*self.db
+
+class Softmax:
+    def forwar(self,z):
+        self.z = z
+        zmax = z.max(axis = 1, keepdims = True)
+        expz = np.exp(z - zmax)
+        Z = expz.sum(axis = 1, keepdims = True)
+        return expz / Z
+    def backward(self, dp):
+        p = self.forwar(self.z)
+        pdp =p * dp
+        return pdp - p * pdp.sum(axis = 1, keepdims = True)
+class CrossEntropyLoss:
+    def forward(self,p,y):
+        self.p = p
+        self.y = Y
+        p_of_y = p[np.arange(len(y)), y]
+        log_prob = np.log(p_of_y)
+        return -log_prob.mean()
+    def backward(self,loss):
+        dlog_softmax = np.zeros_like(self.p)
+        dlog_softmax[np.arange(len(self.y)), self.y] -= 1.0/len(self.y)
+        return dlog_softmax / self.p
